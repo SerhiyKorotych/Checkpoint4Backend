45164493201df1fb2615967c9e347deb3fbf0d92
Serhiy Korotych <serhiykorotych@hotmail.com>
2022-02-03 15:57:08 +0000

    fixed something

--- db-config.js
+++ db-config.js
@@ -2,11 +2,11 @@ require("dotenv").config();
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost", // address of the server
-  port: 4000, // port of the DB server (mysql), not to be confused with the nodeJS server PORT !
-  user: "root",
-  password: "Sesewwe1",
-  database: "movies",
+  host: process.env.DB_HOST, // address of the server
+  port: process.env.DB_PORT, // port of the DB server (mysql), not to be confused with the nodeJS server PORT !
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 
 module.exports = connection;
